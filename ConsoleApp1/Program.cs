5f30ea12cd9e66a83329891ec86ef1c90c25d85d
KceroStudios <kcerostudios@gmail.com>
2023-11-29 13:01:01 -0500

    if else, exercise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DesicionMaking
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("How is the temperature");
+            string temperature = Console.ReadLine();
+
+            int numericTemperature = int.Parse(temperature);
+    
+            if (numericTemperature <= 10)
+            {
+                Console.WriteLine("You Need a Coat, la temperatura es de " + numericTemperature);
+            }
+            if (numericTemperature > 10 && numericTemperature < 20)
+            {
+                Console.WriteLine("It's a good weather, la temperatura es de " + numericTemperature);
+            }
+            else { Console.WriteLine("it's so Hot, la temperatura es de " + numericTemperature);
+            }
+       
+
+        }
+    }
+}
