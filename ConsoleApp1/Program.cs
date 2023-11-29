5f06229e503362e54557ba40cef993fa37d986c6
KceroStudios <kcerostudios@gmail.com>
2023-11-29 13:29:45 -0500

    tryParse Exersise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,14 +6,24 @@
         {
             Console.WriteLine("How is the temperature");
             string temperature = Console.ReadLine();
+            int numericTemperature;
 
-            int numericTemperature = int.Parse(temperature);
+            int number;
+            if (int.TryParse(temperature, out number))
+            {
+                numericTemperature = number;
+            }
+            else
+            {
+                numericTemperature = 0;
+                Console.WriteLine("you did not enter a valid number; the default temperature is set to 0. ");
+            }
     
             if (numericTemperature <= 10)
             {
                 Console.WriteLine("You Need a Coat, la temperatura es de " + numericTemperature);
             }
-            if (numericTemperature > 10 && numericTemperature < 20)
+            else if (numericTemperature > 10 && numericTemperature < 20)
             {
                 Console.WriteLine("It's a good weather, la temperatura es de " + numericTemperature);
             }
